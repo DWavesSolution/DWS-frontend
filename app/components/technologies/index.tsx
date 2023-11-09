81e52d840fc088143072d34c1003967070a4a7ee
Raza Mughal <razasabir23outlook.com>
2023-11-09 23:33:27 +0500

    resonsive header created

--- app/components/technologies/index.tsx
+++ app/components/technologies/index.tsx
@@ -3,6 +3,7 @@ import { Technologies_Tabs } from "@/app/static-data/data";
 import { Box, Container, Tab, Tabs } from "@mui/material";
 import React, { useState } from "react";
 import DWSImage from "../DWSImage";
+import { styles } from "./styles";
 
 const Technologies = () => {
   const [selectedTab, setSelectedTab] = useState(0);
@@ -10,35 +11,37 @@ const Technologies = () => {
     setSelectedTab(newValue);
   };
   return (
-    <Container>
-      <Tabs
-        value={selectedTab}
-        onChange={handleTabChange}
-        centered
-        sx={{ color: "black" }}
-      >
-        {Technologies_Tabs.map((item: any, index) => {
-          return (
-            <Tab sx={{ color: "black" }} key={index}>
-              {item.TechName}
-            </Tab>
-          );
-        })}
-      </Tabs>
-      <div>
-        {Technologies_Tabs[selectedTab].content.map((item: any, index) => {
-          return (
-            <Box sx={{ height: 100, width: 100 }} key={index}>
-              <DWSImage
-                src={item.logo}
-                alt={item.title}
-                style={{ objectFit: "contain" }}
-              />
-            </Box>
-          );
-        })}
-      </div>
-    </Container>
+    <Box sx={styles.MainBox}>
+      <Container>
+        <Tabs
+          value={selectedTab}
+          onChange={handleTabChange}
+          centered
+          sx={{ color: "black" }}
+        >
+          {Technologies_Tabs.map((item: any, index) => {
+            return (
+              <Tab sx={{ color: "black" }} key={index}>
+                {item.TechName}
+              </Tab>
+            );
+          })}
+        </Tabs>
+        <div>
+          {Technologies_Tabs[selectedTab].content.map((item: any, index) => {
+            return (
+              <Box sx={{ height: 100, width: 100 }} key={index}>
+                <DWSImage
+                  src={item.logo}
+                  alt={item.title}
+                  style={{ objectFit: "contain" }}
+                />
+              </Box>
+            );
+          })}
+        </div>
+      </Container>
+    </Box>
   );
 };
 
---
+++ app/components/technologies/styles.ts
@@ -0,0 +1,5 @@
+export const styles = {
+  MainBox: {
+    margin: "50px 0px",
+  },
+};
