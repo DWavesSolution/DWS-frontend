39c5bd7b97055fa30ffc722f2e0b6bd5f455d447
Raza Mughal <razasabir23outlook.com>
2023-11-25 15:37:55 +0500

    services pages completed

--- app/components/header/styles.ts
+++ app/components/header/styles.ts
@@ -25,7 +25,7 @@ export const styles = {
     alignItems: "center",
     gap: "0.4rem",
     zIndex: "1",
-    background: "linear-gradient(108deg,#f87a9a 25%,#4219bf 50%,#004ca2)",
+    background: "linear-gradient(108deg,#26315b 25%,#127a88 50%,#004ca2)",
     backgroundSize: "200% 100%",
     color: "#fff",
     padding: "0.7rem 1.2rem",
