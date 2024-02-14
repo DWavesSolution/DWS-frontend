39642cd78a0df8a0cd38c0898b792c3560ae78b3
Raza Mughal <razasabir23outlook.com>
2024-02-14 14:29:07 +0500

    sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    siteUrl: "https://digitalwavesolutions.ca/",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://digitalwavesolutions.ca/about-us",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl:
+      "https://digitalwavesolutions.ca/services/digital-product-development",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://digitalwavesolutions.ca/services/social-media-marketing",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl:
+      "https://digitalwavesolutions.ca/services/search-engine-optimization",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://digitalwavesolutions.ca/services/ai-ml",
+    generateRobotsTxt: true,
+  },
+  {
+    siteUrl: "https://digitalwavesolutions.ca/contact-us",
+    generateRobotsTxt: true,
+  },
+];
